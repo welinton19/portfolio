a9468c65268bf5baa392882da2267f0811fc7c34
welinton19 <133980158+welinton19@users.noreply.github.com>
2023-10-10 16:22:41 -0300

    ajustando pequenos erros de escrita

--- src/componenetes/secoes/Presentatio.js
+++ src/componenetes/secoes/Presentatio.js
@@ -8,7 +8,7 @@ function Presentation(){
             <h4><strong>Bem-vindo ao meu Portifolio</strong></h4>
             <h1>Olá, eu sou Welinton Gomes!</h1>
             <p>Sou fissurado em tecnologia e soluções que ajudam a socieadade como um todo.<br/>
-               Como Desenvolvndo Front-End busco me aprimora esta sempre por dentro das novidades,<br/>
+               Como Desenvolvedor Front-End busco me aprimora esta sempre por dentro das novidades,<br/>
                para poder ajudar a todos. A tecnologia é algo inovador por isso estou sempre curioso <br/>
                e com muita fome de aprender sobre esse
                universo que é impressionante.E buscando sempre me<br/> 
